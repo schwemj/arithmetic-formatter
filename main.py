3f5afebb68de8e78531a5ee45f44b26f079c2f5b
schwemj <129846616+schwemj@users.noreply.github.com>
2023-08-02 19:20:50 +0200

    Upload main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from arithmetic_formatter import arithmetic_formatter
+
+print('Example code:')
+print(arithmetic_formatter(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", '2 - 1000'],display_answers=True))
+
+
